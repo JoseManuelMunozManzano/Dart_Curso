a04d1344561d521d88170cac36ae81b0f11c94ea
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-10-11 06:39:21 +0200

    01-Basico - Datatypes - String

--- 01_basico/02_datatypes.dart
+++ 01_basico/02_datatypes.dart
@@ -1,5 +1,6 @@
 main() {
-  // Números
+//? ============ Números
+  var x = 1; // Mejor especificar el tipo de dato concreto
   int a = 10;
   double b = 5.5;
 
@@ -13,4 +14,38 @@ main() {
   double $b = 40;
 
   print(_a + $b); // int + double --> double
+
+  //? ============ Strings
+  var nombreX = 'Tony';
+  // Se puede usar comilla simple o doble. Se suele usar la comilla simple
+  String nombre = "Tony";
+
+  print(nombreX == nombre);
+  print(nombre);
+
+  // Null Safety
+  String? nombre2;
+  print(nombre2);
+
+  // Escape de caracteres
+  String nombre3 = 'O\'Connor';
+  print(nombre3);
+
+  // Usando comillas dobles cuando en el texto hay comillas simples
+  String nombre3X = "O'Connor";
+
+  // Strings multilinea usando triple comilla simple o triple comilla doble.
+  // Se usa la variable nombre, usando $, dentro del string multilinea.
+  String multilinea = '''
+  Hola Mundo
+  ¿Cómo estás?
+  $nombre
+  O'Connor''';
+
+  print(multilinea);
+
+  // Se pueden usar variables dentro de cualquier string
+  String apellido = 'Stark';
+  String nombreCompleto = '$nombre $apellido';
+  print(nombreCompleto);
 }
