a8bc41b9c3a4b9faeb67e5f8f2a3aa9821cc65a0
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-10-12 10:25:17 +0200

    02-Comentarios en Dart

---
+++ 01_basico/04_comentarios.dart
@@ -0,0 +1,30 @@
+/**
+ * 
+ * Este archivo tiene por objetivo explicar los comentarios en Dart 
+ * [creadoEn]: 12/10/2023
+ * [programador]: JMMM
+ */
+
+main() {
+  // Comentario de una sola línea
+  int a = 10; // A debería ser 10
+
+  /* Comentario de bloque
+  final personas = [
+    // Esta es la primera persona
+    'José', // [0]
+
+    // Esta es la segunda persona
+    'Adriana', // [1]
+
+    // Esta es la tercera persona
+    'Pepe' // [2]
+  ];
+  */
+}
+
+// Para documentación (Dart Doc) se puede usar el triple slash o para multilinea, /** */
+
+/// Funciona para saludar
+/// Recibe un [nombre] y se concatena con el [mensaje]
+saludar(int nombre, int mensaje) {}
