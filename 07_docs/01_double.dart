ed17e7aaf59d0aa69450ae57ef6e4a4bc4caeea2
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-10-14 18:12:43 +0200

    08-Docs - double - Propiedades y métodos

--- 07_docs/01_double.dart
+++ 07_docs/01_double.dart
@@ -2,7 +2,10 @@ main() {
   // Con esta instrucción estamos creando una instancia de double,
   // y tenemos a nuestra disposición todas las propiedades y métodos
   // que la clase double posee.
-  double numero = 3.1416;
+  double numero = 2.1416;
+
+  // Uso de propiedades estáticas.
+  double infinito = double.infinity;
 
   // Usar el print sobre numero nos lleva al método .toString()
   print('$numero');
@@ -11,4 +14,18 @@ main() {
   // https://api.dart.dev/stable/3.1.3/dart-core/double-class.html
   // Y en concreto: https://api.dart.dev/stable/3.1.3/dart-core/double/sign.html
   print('Firma: ${numero.sign} :: $numero');
+
+  //? Otras propiedades y métodos
+  print('isFinite: ${numero.isFinite} :: $numero');
+  print('isFinite: ${infinito.isFinite} :: $infinito');
+
+  print('abs: ${numero.abs()} :: $numero');
+
+  print('ceil: ${numero.ceil()} :: $numero');
+  print('ceilToDouble: ${numero.ceilToDouble()} :: $numero');
+
+  print('round: ${numero.round()} :: $numero');
+  print('roundToDouble: ${numero.roundToDouble()} :: $numero');
+
+  print('clamp: ${numero.clamp(1, 3)} :: $numero');
 }
