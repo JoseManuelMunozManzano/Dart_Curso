dba5e48467aa2f8cbda22a1eb93d2bec0312e6fb
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-10-15 07:31:46 +0200

    09-Paquetes - Optimizaciones

--- paquetes/lib/classes/reqres_respuesta.dart
+++ paquetes/lib/classes/reqres_respuesta.dart
@@ -8,6 +8,8 @@
 
 import 'dart:convert';
 
+import 'package:paquetes/classes/persona.dart';
+
 ReqResRespuesta reqResRespuestaFromJson(String str) =>
     ReqResRespuesta.fromJson(json.decode(str));
 
@@ -44,37 +46,5 @@ class ReqResRespuesta {
         'total': total,
         'total_pages': totalPages,
         'data': List<dynamic>.from(data.map((x) => x.toJson())),
-      };
-}
-
-class Persona {
-  int id;
-  String email;
-  String firstName;
-  String lastName;
-  String avatar;
-
-  Persona({
-    required this.id,
-    required this.email,
-    required this.firstName,
-    required this.lastName,
-    required this.avatar,
-  });
-
-  factory Persona.fromJson(Map<String, dynamic> json) => Persona(
-        id: json['id'],
-        email: json['email'],
-        firstName: json['first_name'],
-        lastName: json['last_name'],
-        avatar: json['avatar'],
-      );
-
-  Map<String, dynamic> toJson() => {
-        'id': id,
-        'email': email,
-        'first_name': firstName,
-        'last_name': lastName,
-        'avatar': avatar,
       };
 }
--- paquetes/lib/paquetes.dart
+++ paquetes/lib/paquetes.dart
@@ -1,1 +1,25 @@
+// Primero importamos paquetes core de Dart, luego paquetes de
+// terceros (del repositorio) y por último nuestros paquetes.
+import 'package:http/http.dart' as http;
 
+import 'package:paquetes/classes/reqres_respuesta.dart';
+
+void getReqRespService() {
+  final url = Uri.parse('https://reqres.in/api/users?page=2');
+  http.get(url).then((res) {
+    // Extraer y utilizar el body
+    // Necesito convertirlo, usando el paquete dart:convert
+    // Sabemos que un json al final es un mapa, así que aunque veamos dynamic, es un mapa.
+    //
+    //final body = jsonDecode(res.body);
+    // print(body);
+    // print('page: ${body['page']}');
+    // print('per_page: ${body['per_page']}');
+    // print('id del tercer elemento: ${body['data'][2]['id']}');
+
+    final resReqResponse = reqResRespuestaFromJson(res.body);
+    print('page: ${resReqResponse.page}');
+    print('per_page: ${resReqResponse.perPage}');
+    print('id del tercer elemento: ${resReqResponse.data[2].id}');
+  });
+}
